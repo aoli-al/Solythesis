f33f7ef2546646266cd8f718b8f839d2a24ea39d
Leeleo3x <leeleo3x@gmail.com>
2019-07-13 00:38:45 +0800

    update.

--- scripts/bash/setup_bench.sh
+++ scripts/bash/setup_bench.sh
@@ -6,6 +6,4 @@ pip3 install progressbar
 pip3 install py-solc
 pip3 install web3==v5.0.0-beta.2
 pip3 install psrecord
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo apt update
-sudo apt install -y solc
+sudo snap install solc
