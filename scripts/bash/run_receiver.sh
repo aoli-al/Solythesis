ed35d3c5fa6467d7e066358b7575c722b62173c6
Leeleo3x <leeleo3x@gmail.com>
2019-07-13 01:32:32 +0800

    update.

--- scripts/bash/run_receiver.sh
+++ scripts/bash/run_receiver.sh
@@ -2,7 +2,7 @@
 
 ~/parity-ethereum/target/release/parity --accounts-refresh=0 \
    --fast-unlock --no-warp --config \
-   /home/ubuntu/scripts/parity/config.dev-insecure.toml
+   /home/ubuntu/scripts/parity/config.dev-insecure.toml \
    --chain=/home/ubuntu/scripts/parity/config.json  \
    --base-path=/tmp/leo --logging=info  \
    --unsafe-expose --jsonrpc-cors=all
