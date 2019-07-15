81ed4f37967b48e666538f48f62b62eb6c550d9b
Leeleo3x <leeleo3x@gmail.com>
2019-07-15 13:26:04 +0800

    update.

--- scripts/py/visual.py
+++ scripts/py/visual.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import sys
+import re
 from utils import generate_tests
 
 m = {}
@@ -15,6 +16,29 @@ def name_mapping(name):
     return "O"
 
 
+def generate_table(m):
+    corder = ["erc20", "tran", "batchTransfer", "v", "erc721", "tran"]
+    rorder = ["S", "S-N", "O"]
+    for r in rorder:
+        s = " & \\textbf{%s}"% r
+        for c in corder:
+            s += "& %.3f\\%%" % m[r][c]
+        s += "\\\\"
+        print(s)
+
+
+for i in range(6):
+    for benchmark in generate_tests(i):
+        name = benchmark[0] + "-" + benchmark[2]
+        idx = name_mapping(name)
+        if idx not in m:
+            m[idx] = {}
+        f = open("/data/{}/{}.log".format(name, name))
+        for line in f:
+            result = re.match(r"Cumulative writes:.+ingest: (\d*\.?\d*) GB", line)
+            m[idx][benchmark[2]] = float(result.group(1))
+generate_table(m)
+
 for i in range(6):
     for benchmark in generate_tests(i):
         name = benchmark[0] + "-" + benchmark[2]
@@ -25,15 +49,7 @@ for i in range(6):
         res = res[[1]]
         print(np.mean(res).values[0])
         m[idx][benchmark[2]] = np.mean(res).values[0]
+generate_table(m)
 
 
-corder = ["erc20", "tran", "batchTransfer", "v", "erc721", "tran"]
-rorder = ["S", "S-N", "O"]
 
-
-for r in rorder:
-    s = " & \\textbf{%s}"% r
-    for c in corder:
-        s += "& %.3f\\%%" % m[r][c]
-    s += "\\\\"
-    print(s)
