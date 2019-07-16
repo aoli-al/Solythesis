baecc1b1513a042ca88623eac90c712bca6c55f7
Leeleo3x <leeleo3x@gmail.com>
2019-07-16 08:49:28 +0800

    udpate.

--- scripts/bash/run_receiver_no_receiver.sh
+++ scripts/bash/run_receiver_no_receiver.sh
@@ -8,18 +8,18 @@ nohup ~/parity-ethereum/target/release/parity --accounts-refresh=0 \
    --unsafe-expose --jsonrpc-cors=all &
 
 python3 $2 ws://127.0.0.1:8546 ~/scripts/data/$3.csv\
- ~/tests/$1.sol ~/scripts/keys/leo123leo456 ~/scripts/keys/leo123leo987 &
+ ~/tests/$1.sol ~/scripts/keys/leo123leo456 ~/scripts/keys/leo123leo987 --pow
 replay=$!
 sleep 1
 # sudo blktrace -d /dev/xvdf -o $2-$4 &
-python3 ~/scripts/py/miner.py ws://127.0.0.1:8546 &
-miner=$!
-wait $replay
+#python3 ~/scripts/py/miner.py ws://127.0.0.1:8546 &
+#miner=$!
+#wait $replay
 killall parity
 sleep 5
-kill $miner
+#kill $miner
 killall -9 parity
 
-#~/parity-ethereum/target/release/parity export blocks \
-#  --config ~/scripts/parity/config.dev-insecure.toml  --chain ~/scripts/parity/config.json \
-# --base-path=/tmp/parity ~/results/$1-$3-mainchain.bin
+~/parity-ethereum/target/release/parity export blocks \
+  --config ~/scripts/parity/config.dev-insecure.toml  --chain ~/scripts/parity/config.json \
+ --base-path=/tmp/parity ~/results/$1-$3-mainchain.bin
