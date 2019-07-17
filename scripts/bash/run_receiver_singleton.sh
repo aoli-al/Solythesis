79ccd068fe857de8a2244ff75562e02b61fc8534
Leeleo3x <leeleo3x@gmail.com>
2019-07-17 08:08:11 +0000

    update.

--- scripts/bash/run_receiver_singleton.sh
+++ scripts/bash/run_receiver_singleton.sh
@@ -11,7 +11,7 @@ mkdir results
 
 sleep 5
 python3 $2 ws://127.0.0.1:8546 ~/scripts/data/$3.csv\
-  ~/tests/$1.sol ~/scripts/keys/leo123leo456 ~/scripts/keys/leo123leo987 --pow
+  ~/tests/$1.sol ~/scripts/keys/leo123leo987 ~/scripts/keys/leo123leo456 --pow
 replay=$!
 sleep 1
 # sudo blktrace -d /dev/xvdf -o $2-$4 &
