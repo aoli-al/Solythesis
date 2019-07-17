6b52ef28441607d08658e6c52c5fc5b5da6e6917
Leeleo3x <leeleo3x@gmail.com>
2019-07-17 05:33:06 +0000

    update.

--- scripts/bash/run_receiver_no_receiver.sh
+++ scripts/bash/run_receiver_no_receiver.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-nohup ~/parity-ethereum/target/release/parity --accounts-refresh=0 \
-   --fast-unlock --no-warp --config --no-consensus \
-   /home/ubuntu/scripts/parity/config.dev-insecure.toml \
-   --chain=/home/ubuntu/scripts/parity/foundation.json  \
-   --base-path=/tmp/parity --logging=info  \
-   --unsafe-expose --jsonrpc-cors=all &
+~/parity-ethereum/target/release/parity --accounts-refresh=0 \
+   --fast-unlock --no-warp --no-consensus --config \
+   ~/scripts/parity/config.dev-insecure.toml \
+   --chain ~/scripts/parity/foundation.json  \
+   --base-path=/home/leo/fullnode --logging=info  \
+   --unsafe-expose --jsonrpc-cors=all  \
+   --db--path=/home/leo/fullnode/chains/ethereum/db/906a34e69aec8c0d &
 
+sleep 5
 python3 $2 ws://127.0.0.1:8546 ~/scripts/data/$3.csv\
- ~/tests/$1.sol ~/scripts/keys/leo123leo456 ~/scripts/keys/leo123leo987 --pow
+  ~/tests/$1.sol ~/scripts/keys/leo123leo456 ~/scripts/keys/leo123leo987 --pow
 replay=$!
 sleep 1
 # sudo blktrace -d /dev/xvdf -o $2-$4 &
