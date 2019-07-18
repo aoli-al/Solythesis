965fdbd3971e9656abd29f6895e38e6ff9cb6920
Leeleo3x <leeleo3x@gmail.com>
2019-07-18 08:14:34 +0000

    udpate.

--- scripts/bash/test_sync.sh
+++ scripts/bash/test_sync.sh
@@ -7,7 +7,7 @@ mkdir ~/results
 # sudo rm $2-$4.blktrace.*
 nohup ~/parity-ethereum/target/release/parity --accounts-refresh=0 --fast-unlock --no-warp \
   --config ~/scripts/parity/config.dev-insecure.toml  --chain ~/scripts/parity/foundation.json\
-  --base-path=/home/leo/fullnode --no-discovery  --unsafe-expose  --bootnodes=enode://5a3bf48fcb9dd123b50232b360e73c31bdc0992a6dd3bff1ed8efc7766856cc90f5127dd8102d4ca07b168a97a3c976c7be5a4d2a677b0c27da85733d21ee46a@$4:30303  &
+  --base-path=/home/leo/fullnode --no-discovery  --unsafe-expose  --bootnodes=enode://20c9dba148de1201f80798f7fa178a795709bc43dcfcb22a79615c11a3f38886d0ba41c59420e15fb17c0098fba9c8b6c21315bcd43e38e0c32a77bc77dcb3b7@$4:30303  &
 parity_pid=$!
 ~/.local/bin/psrecord $parity_pid --interval 0.1 --log ~/results/$1-$3.txt
 psrecord=$!
