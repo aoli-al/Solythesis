62044d26dc07ae7f2bb158a9b2e5d91e6da5ce62
Leeleo3x <leeleo3x@gmail.com>
2019-07-18 13:13:42 +0800

    update.

--- scripts/bash/run_receiver.sh
+++ scripts/bash/run_receiver.sh
@@ -5,10 +5,10 @@ nohup ~/parity-ethereum/target/release/parity --accounts-refresh=0 \
    /home/ubuntu/scripts/parity/config.dev-insecure.toml \
    --chain=/home/ubuntu/scripts/parity/config.json  \
    --base-path=/tmp/parity --logging=info  \
-   --unsafe-expose --jsonrpc-cors=all &
+   --unsafe-expose --jsonrpc-cors=all --no-discovery  &
 
 python3 $2 ws://127.0.0.1:8546 ~/scripts/data/$3.csv\
- ~/tests/$1.sol ~/scripts/keys/leo123leo456 ~/scripts/keys/leo123leo987 &
+ ~/tests/$1.sol ~/scripts/keys/leo123leo987 ~/scripts/keys/leo123leo456 &
 replay=$!
 sleep 1
 # sudo blktrace -d /dev/xvdf -o $2-$4 &
